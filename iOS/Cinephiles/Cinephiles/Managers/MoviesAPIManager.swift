c5cbab5845ee0a4273f82dc9c306f832835f3764
Haroldo Gondim <haroldo.gondim@gmail.com>
2018-07-20 21:01:16 -0300

    Add API constants

--- iOS/Cinephiles/Cinephiles/Managers/MoviesAPIManager.swift
+++ iOS/Cinephiles/Cinephiles/Managers/MoviesAPIManager.swift
@@ -13,12 +13,12 @@ class MoviesAPIManager: NSObject {
     class func getPopupar(page: Int, completion: @escaping (_ list: [Movies], _ error: Error?) -> ()) {
 
         let parameters: Parameters = [
-            "api_key": "1f54bd990f1cdfb230adb312546d765d",
-            "language": "en-US",
+            "api_key": Constants.API_KEY,
+            "language": Constants.API_LANGUAGE,
             "page": page
         ]
         
-        let url = "https://api.themoviedb.org/3/movie/popular"
+        let url = Constants.API_URL + "/movie/popular"
         
         Alamofire.request(url, method: .get, parameters: parameters).responseObject { (response: DataResponse<MoviesList>) in
             
--- iOS/Cinephiles/Cinephiles/Util/Constants.swift
+++ iOS/Cinephiles/Cinephiles/Util/Constants.swift
@@ -5,8 +5,10 @@
 //  Created by Haroldo Gondim on 20/07/18.
 //
 
-import UIKit
+struct Constants {
 
-class Constants: NSObject {
+    static let API_URL = "https://api.themoviedb.org/3"
+    static let API_KEY = "1f54bd990f1cdfb230adb312546d765d"
+    static let API_LANGUAGE = "en-US"
 
 }
