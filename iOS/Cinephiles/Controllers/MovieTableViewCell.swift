ef11a50909a8dc9eba9dc98ed0ed3ea18438f149
Haroldo Gondim <haroldo.gondim@gmail.com>
2018-07-22 20:38:55 -0300

    no selection in tableview cell

--- iOS/Cinephiles/Controllers/MovieTableViewCell.swift
+++ iOS/Cinephiles/Controllers/MovieTableViewCell.swift
@@ -15,6 +15,8 @@ class MovieTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        selectionStyle = .none
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
